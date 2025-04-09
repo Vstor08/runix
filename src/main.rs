728dcf4aa528dd6909fa00029be56025f117cff6
vstor <vstor@storozhiloff.online>
2025-04-10 00:04:40 +0500

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+extern crate core;
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
